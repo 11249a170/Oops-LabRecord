47fd0108cad491ff2213e72dc28bc834adea59f9
11249a170@kanchiuniv.ac.in <11249a170@kanchiuniv.ac.in>
2025-11-04 23:56:21 -0800

    Create rectangle.cpp

---
+++ rectangle.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class rectangle{
+    int length,width;
+    public:
+    rectangle(int l,int w){
+    
+        length=l;
+        width=w;
+    }
+    inline int area(){return length*width;}
+};
+int main(){
+    rectangle r1(5,4);
+    rectangle r2(10,7);
+    cout<<"Area of r1:"<<r1.area()<<endl;
+    cout<<"Area of r2:"<<r2.area()<<endl;
+    return 0;
+}
