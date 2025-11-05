13a7979eb29610c48612350fc1f73a75784cad70
11249a170@kanchiuniv.ac.in <11249a170@kanchiuniv.ac.in>
2025-11-04 22:33:24 -0800

    Create car.cpp

---
+++ car.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class car
+{
+    public:
+    string model;
+    string company;
+    float price;
+    void display()
+    {
+        cout<<"Model:"<<model<<endl;
+        cout<<"Company:"<<company<<endl;
+        cout<<"Price:Rs"<<price<<endl;
+    }
+};
+int main()
+{
+    car c1,c2,c3;
+    c1.company="toyota";
+    c1.model="innova";
+    c1.price=200000;
+    c2.company="honda";
+    c2.model="city";
+    c2.price=120000;
+    c3.company="hyundai";
+    c3.model="i20";
+    c3.price=900000;
+    c1.display();
+    c2.display();
+    c3.display();
+    return 0;
+}
