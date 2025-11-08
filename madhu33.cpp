0033a4a623e26d54f9877febb80301d6c0658c2a
11249a170@kanchiuniv.ac.in <11249a170@kanchiuniv.ac.in>
2025-11-07 23:14:29 -0800

    Create madhu33.cpp

---
+++ madhu33.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class Temperature{
+    float celsius;
+    public:
+    void getData{
+        cout<<"Enter temperture in celsius:";
+        cin>>celsius;
+    }
+    void operator(){
+        float fahrenheit=(celsius*9/5)+32;
+        cout<<"Temperature in Fahrenhit:"<<fahrenheit<<endl;
+    }
+};
+int main(){
+    Temperture t;
+    t.getData();
+    -t;
+    return 0;
+}
