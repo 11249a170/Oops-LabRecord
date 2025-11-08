75d8f418de0475ec0306637bbed5cede8fb064c1
11249a170@kanchiuniv.ac.in <11249a170@kanchiuniv.ac.in>
2025-11-07 23:15:15 -0800

    Create madhu34.cpp

---
+++ madhu34.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class complex{
+    float real,imag;
+    public:
+    void getData(){
+        cout<<"Enter real and imaginary parts:";
+        cin>>real>>imag;
+    }
+    complex operator+(complex c){
+        complex temp;
+        temp.real=real+c.real;
+        temp.imag=imag+c.imag;
+        return temp;
+    }
+    void display(){
+        cout<<real<<"+"<<imag<<"i"<<endl;
+    }
+};
+int main(){
+    complex c1,c2,c3;
+    c1.getData();
+    c2.getData();
+    cout<<"sum=";
+    c3.display;
+    return 0;
+}
